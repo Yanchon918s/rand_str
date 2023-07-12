74956ef432336a495704448890ac0e21282c8ab5
ao9s <124195190+ao9s@users.noreply.github.com>
2023-07-12 12:46:58 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+function generate() {
+    let length = parseInt(document.getElementById('length').value);
+    let quantity = parseInt(document.getElementById('quantity').value);
+    let output = document.getElementById('output');
+    let downloadLink = document.getElementById('downloadLink');
+    let strings = new Set();
+
+    while (strings.size < quantity) {
+        let randomString = Math.random().toString(36).substring(2, 2 + length);
+        if (!strings.has(randomString)) {
+            strings.add(randomString);
+        }
+    }
+
+    let text = Array.from(strings).join('\n');
+    output.innerText = text;
+
+    let file = new Blob([text], { type: 'text/plain' });
+    downloadLink.href = URL.createObjectURL(file);
+    downloadLink.download = 'strings.txt';
+    downloadLink.style.display = 'block';
+}
